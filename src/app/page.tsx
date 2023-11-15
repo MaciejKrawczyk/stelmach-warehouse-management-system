6f333edc9bfd7f88af611d2e73c2643c7cb7c92c
MaciejKrawczyk <kontakt.maciejkrawczyk@gmail.com>
2023-11-15 09:58:55 +0100

    adding item-magazine to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import {server} from "@/backend/trpc/server";
+import Link from "next/link";
 
 export default async function Home() {
   
@@ -6,8 +7,22 @@ export default async function Home() {
   console.log(data)
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <main className="flex justify-center items-center">
+      
+      <Link
+        href={'/item-magazine'}
+        className="m-5 px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
+      >
+        Go to Item Magazine
+      </Link>
+      
+      <Link
+        href={'/file-opener'}
+        className="m-5 px-6 py-3 text-lg font-semibold text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 focus:ring-opacity-50"
+      >
+        Open File Opener
+      </Link>
+      
     </main>
   )
 }
